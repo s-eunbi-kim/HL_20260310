3009d3c63f6b108ccd5c6b35e85b014f78de0ce1
Eunbi Kim <86344770+s-eunbi-kim@users.noreply.github.com>
2026-03-11 11:01:45 +0900

    Update calc.h

--- calc.h
+++ calc.h
@@ -5,12 +5,12 @@
 
 int answer(void);
 bool isPositive(int);
+bool arePositive(int, int);
 int add(int, int);
 int subtract(int, int);
 int divide(int, int);
 int multiply(int, int);
 bool isEven(int);
-// void greet(const char*, char*, size_t);
 const char* greet(const char*);
 
 #endif
